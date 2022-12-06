c68cde7117211b38276e95b41fba2b9f8c03e767
Abhinav Salgania <abhinavsalgania@gmail.com>
2022-12-06 01:26:48 -0330

    add toggle functionality

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
 let gridContainer = document.querySelector(".gridContainer");
+let currentlyActive = true;
 
-function createGrid(size)
-{
-    for (let i = 0; i < size*size; i++) 
-        {
-            gridContainer.style.gridTemplateColumns = "repeat(${size}, 1fr)";
-            gridContainer.style.gridTemplateRows = "repeat(${size}, 1fr)";
+function createGrid(size){
 
-                let square = document.createElement("div");
-                square.style.backgroundColor = "yellow";
-                square.style.border = "1px solid black";
-
-                square.addEventListener('mouseenter', function(e) {
-                    e.target.style.backgroundColor = "black";
-                    });
-
-                gridContainer.insertAdjacentElement("beforeend", square);
-            }
+for (let i = 0; i < size*size; i++) {
+    gridContainer.style.gridTemplateColumns = "repeat(${size}, 1fr)";
+    gridContainer.style.gridTemplateRows = "repeat(${size}, 1fr)";
+    let square = document.createElement("div");
+    square.style.backgroundColor = "yellow";
+    square.style.border = "1px solid black";
+    square.addEventListener('mouseenter', colorSquare);
+    gridContainer.insertAdjacentElement("beforeend", square);
+    }
 }
 
-createGrid(16); 
+createGrid(16);
+
+function colorSquare(){
+    if(currentlyActive)
+        this.style.backgroundColor = "black";
+    else
+        this.style.backgroundColor = "purple";
+}
+
+document.querySelector('body').addEventListener('click', () =>{
+    currentlyActive = !currentlyActive;
+});
